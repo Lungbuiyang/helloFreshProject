ae651ed6f822fe94ae0584d00e58e0d7723f41e2
unknown <lungbuiyangtpanmei@gmail.com>
2023-04-30 08:59:37 +0530

    first commit

--- stylejava.js
+++ stylejava.js
@@ -16,18 +16,23 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
-// dark mode
-// var icon = document.getElementById("moonIcon");
-//     icon.onclick = function(){
-//         document.body.classList.toggle("dark-theme");
-//         if(document.body.classList.contains("dark-theme")){
-//             icon.src = "sun.png";
-//         }else{
-//             icon.src = "moon.png";
-//         }
-//     }
 
+// dark mode
 function changeMode(){
     let mybody = document.body;
     mybody.classList.toggle('mydark');
 }
+
+
+// coupon pop up
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        2000
+    )
+});
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
