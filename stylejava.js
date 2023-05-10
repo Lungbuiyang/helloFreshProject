978711a05fbd2f3fd148182821697dd52514dc23
unknown <lungbuiyangtpanmei@gmail.com>
2023-05-11 03:19:39 +0530

    first commit

--- stylejava.js
+++ stylejava.js
@@ -18,21 +18,52 @@ $('.owl-carousel').owlCarousel({
 
 
 // dark mode
-function changeMode(){
+function changeMode() {
     let mybody = document.body;
     mybody.classList.toggle('mydark');
 }
 
 
 // coupon pop up
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     setTimeout(
-        function open(event){
+        function open(event) {
             document.querySelector(".popup").style.display = "block";
         },
         2000
     )
 });
-document.querySelector("#close").addEventListener("click", function(){
+document.querySelector("#close").addEventListener("click", function () {
     document.querySelector(".popup").style.display = "none";
 });
+
+
+// geolocation
+let x = document.getElementById('out');
+let y = document.getElementById('weather');
+function geolocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)
+    } else {
+        x.innerText = "Geo Not Supported"
+    }
+}
+
+function showPosition(data) {
+    console.log(data)
+    let lat = data.coords.latitude
+    let lon = data.coords.longitude
+    // x.innerText = `Latitude is ${lat} and longitude is ${lon}`
+    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`;
+    //api calling
+    fetch(url, { method: 'GET' })
+        //return promise
+        .then((res) => res.json())
+        //resolve the promise
+        .then((data) => {
+            console.log(data)
+            let cityName = data.city.name;
+            let temp = data.list[0].temp.day + " °C"
+            y.innerText = `${cityName} ${temp}`
+        })
+}
